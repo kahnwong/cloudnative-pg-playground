ec62fd87b9090755934509f67a7c5b33e7738278
Karn Wong <karn@karnwong.me>
2026-05-13 15:26:27 +0700

    upgrade os

--- lab-postgres-upgrade.sh
+++ lab-postgres-upgrade.sh
@@ -12,3 +12,9 @@ kubectl apply -f postgres.yaml
 # bump major version
 sd 17.9 18.2 postgres.yaml
 kubectl apply -f postgres.yaml
+
+# upgrade os
+# # need to upgrade major version via the same base os first
+# # ex. bump v17 to v18 on bookworm, then while on v18, upgrade os from bookworm to trixie
+sd bookworm trixie postgres.yaml
+kubectl apply -f postgres.yaml
